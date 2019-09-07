a7c52c17a753dfdf22bd27bee09f3fd395098e63
unknown <rohityadav8763@gmail.com>
2019-09-07 19:17:24 +0530

    Updates

---
+++ precache-manifest.27659021121c7950f9e81ccb700b9e2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50a7b644524328f73d43ab114862123",
+    "url": "/ims/index.html"
+  },
+  {
+    "revision": "8c29469fbe3aa71c716e",
+    "url": "/ims/static/css/main.73cbf8c2.chunk.css"
+  },
+  {
+    "revision": "04dec1500b2d97241c21",
+    "url": "/ims/static/js/2.2f2643c1.chunk.js"
+  },
+  {
+    "revision": "8c29469fbe3aa71c716e",
+    "url": "/ims/static/js/main.b45b04e9.chunk.js"
+  },
+  {
+    "revision": "1a98ebf383a12d9eba93",
+    "url": "/ims/static/js/runtime~main.97af3cfb.js"
+  }
+]);
